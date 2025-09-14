9bf1de35a7ac53b8e822fa96eed2b0a484eb5e4c
Gabrovez <gabrovez321@gmail.com>
2025-09-14 13:52:14 +0300

    //Задание 7

--- App.kt
+++ App.kt
@@ -23,10 +23,16 @@ fun main(){
     println("${n1} - ${n2} = ${n1 - n2}")
     println("${n1} * ${n2} = ${n1 * n2}")*/
     //Задание 6
-    val n = readln().toInt()
+    /*val n = readln().toInt()
     val n1 = n / 100
     val n2= n /10 %10
     val n3= n %10
-    println("$n → $n1 + $n2 + $n3 = ${n1 + n2 + n3}")
+    println("$n → $n1 + $n2 + $n3 = ${n1 + n2 + n3}")*/
+    //Задание 7
+    val m = readln()
+    val n1 = readln()
+    val n2 = readln()
+    val n3 = readln()
+    println("$n1$m$n2$m$n3")
 
 }
