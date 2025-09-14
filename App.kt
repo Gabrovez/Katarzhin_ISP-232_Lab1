063245159d11636d3b0f6cb440f200c92464b284
Gabrovez <gabrovez321@gmail.com>
2025-09-14 13:30:31 +0300

    //Задание 3

--- App.kt
+++ App.kt
@@ -1,12 +1,15 @@
 fun main(){
     //Задание 1
-    val a = "Library_of_Ruina"
+    /*val a = "Library_of_Ruina"
     val b = 11
     val c = 3.14
     val d = "Г"
     println(a)
     println(b)
     println(c)
-    println(d)
-
+    println(d)*/
+    //Задание 3
+    val pi = 3.14
+    val rad = 5
+    println("${pi * rad.toDouble() * rad.toDouble()}")
 }
