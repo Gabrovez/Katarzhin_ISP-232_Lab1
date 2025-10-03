ada0286e4d384c96fc3e22205f226d174de411e5
Gabrovez <gabrovez321@gmail.com>
2025-10-03 15:30:49 +0300

    Шаг 13. Инкремент и декремент 2

--- App.kt
+++ App.kt
@@ -149,6 +149,7 @@ fun main(){ // начало функции main
     println("Остаток: ${a % b}")*/
     //Шаг 13. Инкремент и декремент
     var count: Int = 10
+    count = count + 1
     println(count)
 
 
